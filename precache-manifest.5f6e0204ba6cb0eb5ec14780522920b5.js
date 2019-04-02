e5263452ef36c1e5050bc8f15fdf9aa1f7f240e5
Cheree Nielson <38470257+ChereeNielson@users.noreply.github.com>
2019-04-01 18:17:51 -0600

    Updates

---
+++ precache-manifest.5f6e0204ba6cb0eb5ec14780522920b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c3bd79ed0e125202499",
+    "url": "/MemoryGame/static/js/runtime~main.d54f135b.js"
+  },
+  {
+    "revision": "558f598bbec2a7aeacc8",
+    "url": "/MemoryGame/static/js/main.576968ee.chunk.js"
+  },
+  {
+    "revision": "b73afbb738c4fda25a1a",
+    "url": "/MemoryGame/static/js/2.d327fbf7.chunk.js"
+  },
+  {
+    "revision": "558f598bbec2a7aeacc8",
+    "url": "/MemoryGame/static/css/main.eac881d4.chunk.css"
+  },
+  {
+    "revision": "783b9dfce4a0483e706a3cf1599d8a03",
+    "url": "/MemoryGame/index.html"
+  }
+];
